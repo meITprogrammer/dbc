be83f19942ecc2727cd44542939349418f2c1526
Maria <mcr.holgado@gmail.com>
2023-09-14 22:51:42 +1200

    update

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,15 +3,14 @@ import { Link } from 'react-router-dom';
 import "../App.css";
 
 
-function MainPage() {
+function Homepage() {
  return (
  <div align="center">
- <h1> Welcome to FSA Biz Card</h1>
- <button><Link to='/Create' className='link create'>Create New Card</Link></button>
- <button><Link to='/Update' className='link create'>Update Card</Link></button>
- <br/><br/>
+    <h1> Welcome to FSA Biz Card</h1>
+    <button><Link to='/Create' className='link create'>Create New Card</Link></button>
+    <button><Link to='/Update' className='link update'>Update Card</Link></button>
  </div>
  );
 };
-export default MainPage;
+export default Homepage;
 
