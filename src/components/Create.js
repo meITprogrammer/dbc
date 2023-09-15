43554905169d30e4a1b4f5c5b51e41030b30bc40
Maria <mcr.holgado@gmail.com>
2023-09-15 21:22:24 +1200

    updated to form

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,48 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../images/logo_fsa.png"
 import { Link } from "react-router-dom";
 import "../App.css";
 
 function Create() {
+    const [name, setName] = useState();
+    const [job, setJob] = useState();
+    const [company, setCompany] = useState();
+    const [email, setEmail] = useState();
+    const [profilePhoto, setProfilePhoto] = useState();
+    const [companyLogo, setCompanyLogo] = useState();
+    const [password, setPassword] = useState();
+
     return(
     <div>
-        <img src={logo} />
         <div className="main-container">
+            <img src={logo} />
             <h2 className="main-heading main-content">
                 Create a New Business Card
             </h2>
-            <h4 className="main-content">
-                Name    
-                <input className="input-name" placeholder="Enter your full name here"></input>
-            </h4>
-            <h4 className="main-content">
-                Job Title     
-                <input className="input-jobtitle" placeholder=""></input>
-            </h4>
-            <h4 className="main-content">
-                Company Name    
-                <input className="input-company" placeholder=""></input>
-            </h4>
-            <h4 className="main-content">
-                Email Address    
-                <input className="input-email" placeholder="eg hello@futureskills.co.nz"></input>
-            </h4>
-            <h4 className="main-content">
-                Add Profile Photo 
-                <button>Select File</button>
-            </h4>
-            <h4 className="main-content">
-                Add Company Logo 
-                <button>Select File</button>
-            </h4>
-            <h4 className="main-content">
-                Password 
-                <input className="input-password" placeholder=""></input>
-            </h4>
-
-            <div><button className="form-one-next">Submit</button></div>
+            <form>
+                <div class="form-group">
+                    <label for="name">Name:</label>
+                    <input type="text" name="name" className="input-name" placeholder={name}/>
+                </div>
+                <div class="form-group">
+                    <label for="job">Job Title:</label>
+                    <input type="text" name="job" className="input-job" placeholder={job}/>
+                </div>
+                <div class="form-group">
+                    <label for="company">Company:</label>
+                    <input type="text" name="company" className="input-company" placeholder={company}/>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email Address:</label>
+                    <input type="text" name="email" className="input-email" placeholder={email}/>
+                </div>
+                <div class="form-group">
+                    <label for="profilePhoto">Profile Photo: </label>
+                    <button type="text" name="profilePhoto" className="input-profile-photo" placeholder={profilePhoto}>Select Photo</button>
+                </div>
+                <div class="form-group">
+                    <label for="companyLogo">Company Logo:</label>
+                    <button type="text" name="companyLogo" className="input-company-logo" placeholder={companyLogo}>Select Logo</button>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="text" name="password" className="input-password" placeholder={password}/>
+                </div>
+                <div><button className="form-one-next">Submit</button></div>            
+            </form>            
             <div>
-            <button><Link to='/dbc/' className="link backToMain">Back to Main Page</Link></button>
+                <button><Link to='/dbc/' className="link backToMain">Back to Main Page</Link></button>
             </div>
         </div>
     </div>
